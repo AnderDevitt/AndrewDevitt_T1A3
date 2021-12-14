e8c6c429f31c9b6a59a592f04d582ddd09222eb8
AnderDevitt <gcab022116@coderacademy.edu.au>
2021-12-14 21:20:24 +1000

    bundler init and added colorize and tty-prompt gems; functions created for user to set goals and partially enter a workout

--- main.rb
+++ main.rb
@@ -1,32 +1,54 @@
 require "tty-prompt"
 require "colorize"
+require_relative "./target.rb"
 
 #define variables
 $prompt = TTY::Prompt.new
-
+#variables for user goals
 target_exercises = []
 target_reps =[]
+
+
 #shows the main menu and returns the selected option
 def select_main_menu
-    answer = $prompt.select("What would you like to do today?".colorize(:light_blue), ["Weekly goals", "Enter today's workout", "Check progress", "Review the week", "Exit"])
+    answer = $prompt.select("What would you like to do today?".colorize(:light_cyan), ["Set my goals", "Enter today's workout", "Review the week", "Exit"])
     return answer
 end
 
-#show the list of skills and return the users skills
+#selects the exercises the user wants to do
 def select_exercises
-    answer = $prompt.multi_select("Select the three exercises for this week.".colorize(:light_blue), ["Pushups", "Tricep presses", "Situps", "Crunches", "Legraises", "Lunges", "Squats"])
+    system = "clear"
+    #display a multiple-choice menu of exercises
+    answer = $prompt.multi_select("Select the three exercises for this week.".colorize(:light_cyan), ["Pushups", "Tricep presses", "Situps", "Crunches", "Legraises", "Lunges", "Squats"])
     return answer
 end
 
+#sets the number of repetitions the user will aim to do each day
 def set_reps(goals)
+    system = "clear"
     array = []
+    #loops through selected exercises and for each displays the prompt below
     goals.each do |i|
-        #print "How many #{i} will you do? "
-        #array << gets.chomp.to_i
-        answer = $prompt.ask("How many #{i} will you do: 0-100?".colorize(:light_blue)) { |q| q.in("0-100") }
+        #prompt the user for the number of repetitions for an exercise. Displays error message if number not between 1 and 100
+        answer = $prompt.ask("How many #{i} will you do each day: 1-100?".colorize(:light_cyan)) { |q| q.in("1-100") }
+        #fills an array
         array << answer
     end
     return array
+end
+
+#enter the workout for the day
+def enter_workout(exercises)
+    system = "clear"
+    array = []
+    #loops through exercises and for each displays the prompt below
+    exercises.each do |i|
+        #prompt the user for the number of repetitions done today for an exercise. Displays error message if number not between 1 and 100
+        answer = $prompt.ask("How many #{i} did you do today: 1-100?".colorize(:light_cyan)) { |q| q.in("1-100") }
+        #fills an array
+        array << answer
+    end
+    puts array
 end
 
 system "clear"
@@ -39,17 +61,19 @@ while option != "Exit"
     option = select_main_menu
     #case statement to handle the options of the menu
     case option
-    when "Weekly goals"
-        puts "Weekly goals selected"
+    when "Set my goals"
+        #get user exercise choices from function
         target_exercises = select_exercises
+        #get number of repetitions user will aim for from function
         target_reps = set_reps(target_exercises)
-        puts target_exercises
-        puts target_reps
-        #puts array
+        #testing: later print with for loop
+        #puts target_exercises
+        #puts target_reps
+        #target.show_goals
+        
     when "Enter today's workout"
-        puts "Entering the workout"
-    when "Check progress"
-        puts"This week so far"
+        enter_workout(target_exercises)
+
     when "Review the week"
         puts "congratulations!"
     else
