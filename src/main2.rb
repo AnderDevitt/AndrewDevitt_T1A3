ccdddbbbcb16c97bc57426d1ef47497ef392410c
AnderDevitt <gcab022116@coderacademy.edu.au>
2021-12-16 16:02:41 +1000

    can now draw the output table. Need to read data from files into arrays and then into hashes (or straight into hashes

--- src/main2.rb
+++ src/main2.rb
@@ -22,18 +22,17 @@ end
 
 #selects the exercises the user wants to do
 def select_exercises
-    #system = "clear"
+    #space 
     puts ""
-    #display a multiple-choice menu of exercises; space added to strings for formatting
+
+    #display a multiple-choice menu of exercises and saves choices to a file
     exercise = $prompt.multi_select("Select the three exercises for this week.".colorize(:light_cyan), ["Pushups", "Tricep pressess", "Situps", "Crunches", "Leg-raises", "Lunges", "Squats"])
     #create a file to store an array for exercises
     File.open("./saves/exercises.txt", "w") do |f|
         exercise.each { |element| f.puts(element) }
     end
 
-
-#sets the number of repetitions the user will aim to do each day
-
+    #sets the number of repetitions the user will aim to do each day and saves in a file
     array = []
     #loops through selected exercises and for each displays the prompt below
     exercise.each do |i|
@@ -48,6 +47,7 @@ def select_exercises
         array.each { |element| f.puts(element) }
     end
 end
+
 #enter the workout for the day
 def enter_workout()
     system = "clear"
@@ -78,7 +78,55 @@ def enter_workout()
 end
 
 def review_week
-    puts "congratulations!"
+    array = []
+    #get the names of files in the save directory
+    #array = Dir[ './saves/*' ].select{ |f| File.file? f }.map{ |f| File.basename f }
+    #print array
+
+    arr = [{Ex: "ex1", Go: "20", Mon: "3", Tue: "3", Wed: "3", Thu: "3", Fri: "3"},
+        {Ex: "ex2", Go: "12", Mon: "12", Tue: "12", Wed: "12", Thu: "12", Fri: "12"},
+        {Ex: "ex3", Go: "4", Mon: "4", Tue: "5", Wed: "5", Thu: "5", Fri: "5"}]
+        
+    #FOLLOWING 30 LINES TO CREATE A TABLE FROM: https://stackoverflow.com/questions/28684598/print-an-array-into-a-table-in-ruby
+    col_labels = { Ex: "Exercises", Go: "My Goals", Mon: "Monday", Tue: "Tuesday", Wed: "Wednesday", Thu: "Thursday", Fri: "Friday" }
+
+        @columns = col_labels.each_with_object({}) { |(col,label),h|
+            h[col] = { label: label,
+                       width: [arr.map { |g| g[col].size }.max, label.size].max } }
+          
+          def write_header
+            puts "| #{ @columns.map { |_,g| g[:label].ljust(g[:width]) }.join(' | ') } |"
+          end
+          
+          def write_divider
+            puts "+-#{ @columns.map { |_,g| "-"*g[:width] }.join("-+-") }-+"
+          end
+          
+          def write_line(h)
+            str = h.keys.map { |k| h[k].ljust(@columns[k][:width]) }.join(" | ")
+            puts "| #{str} |"
+          end
+
+        write_divider
+        write_header
+        write_divider
+        arr.each { |h| write_line(h) }
+        write_divider
+
+    #open the exercises file and read the exercises into an array
+    # file = File.open("./saves/exercises.txt")
+    # exercises_array=[] # start with an empty array
+    # file.each_line {|line|
+    #     exercises_array.push line
+    # }
+    # #open the targets file and read the exercises into an array
+    # file = File.open("./saves/targets.txt")
+    # targets_array=[] # start with an empty array
+    # file.each_line {|line|
+    #     targets_array.push line
+    # }
+
+
 end
 
 #option = "Set my goals" if ARGV[0] == "goals"
@@ -87,7 +135,7 @@ end
 option = ""
 while option != "Exit"
 
-    #display app heading and greeting
+    #display app heading and greeting on every screen
     system "clear"
     puts font.write("Small Steps")
     puts "" 
@@ -95,36 +143,17 @@ while option != "Exit"
     puts ""
 
     #invokes the menu and stores the option in the variable
-    option = select_main_menu
-
+    option = select_main_menu()
     #case statement to handle the options of the menu
     case option
     when "Set my goals"
         #call a function to choose exercises and target repetitions
-        select_exercises
-
+        select_exercises()
     when "Enter today's workout"
-        # a blank array to hold return from enter_workout function
-        #reps_array = []
-        
-
-        
-        enter_workout
-        #for each day, pass the exercises_array to the function, return the value to a temporary array, and write the data to a file for that day
-        # case day
-        # when "Monday"
-        #     reps_array = enter_workout("mon")    
-        # when "Tuesday"
-        #     reps_array = enter_workout("tue") 
-        # when "Wednesday"
-        #     reps_array = enter_workout("wed")  
-        # when "Thursday"
-        #     reps_array = enter_workout("thu")   
-        # when "Friday"
-        #     reps_array = enter_workout("fri") 
-        # end
-
+        #call a function to enter the day and reps completed
+        enter_workout()
     when "Review the week"
+        #call a function to display all data for the week in a table
         review_week()
     else   
         system "clear"
@@ -132,8 +161,9 @@ while option != "Exit"
         next
     end
     #stop the menu from appearing straight away after responding to an option choice
+    puts ""
+    puts ""
     print "Press Enter key to continue..."
     gets
     system "clear"
-
 end
